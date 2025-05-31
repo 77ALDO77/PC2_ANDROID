6f3c252ba296e34807785680c2d1373f8a55a651
Mipuim <u21314588@utp.edu.pe>
2025-05-31 12:27:58 -0400

    Rename image button and implement image selection functionality in DatosDocenteActivity

--- app/src/main/java/com/example/appclase6/DatosDocenteActivity.kt
+++ app/src/main/java/com/example/appclase6/DatosDocenteActivity.kt
@@ -1,9 +1,12 @@
 package com.example.appclase6
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -21,9 +24,12 @@ class DatosDocenteActivity : AppCompatActivity() {
     private lateinit var txtSueldo:TextInputEditText
     private lateinit var txtHijos:TextInputEditText
     private lateinit var atvSexo:AutoCompleteTextView
+    private lateinit var imgDocenteDatos:Button
     private lateinit var btnModificar:Button
     private lateinit var btnVolver:Button
     private lateinit var btnEliminarDocente:Button
+    private var imageUri: Uri? = null
+    private val PICK_IMAGE = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +40,7 @@ class DatosDocenteActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //
+        //referenciar
         txtCodigo=findViewById(R.id.tvCodigo)
         txtNombre=findViewById(R.id.txtNombreDatos)
         txtPaterno=findViewById(R.id.txtPaternoDatos)
@@ -42,12 +48,18 @@ class DatosDocenteActivity : AppCompatActivity() {
         txtSueldo=findViewById(R.id.txtSueldoDatos)
         txtHijos=findViewById(R.id.txtHijosDatos)
         atvSexo=findViewById(R.id.atvSexoDatos)
+        imgDocenteDatos=findViewById(R.id.imgDocenteDatos)
         btnModificar=findViewById(R.id.btnModificarDocente)
         btnVolver=findViewById(R.id.btnVolverDocente)
         btnEliminarDocente=findViewById(R.id.btnEliminarDocente)
-        //
+        //cargar la vista con los datos
         mostrarDatos()
         //
+        imgDocenteDatos.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, PICK_IMAGE)
+        }
         btnModificar.setOnClickListener {
             //leer controles
             var cod=txtCodigo.text.toString().toInt()
@@ -57,8 +69,9 @@ class DatosDocenteActivity : AppCompatActivity() {
             var sue=txtSueldo.text.toString().toDouble()
             var hijos=txtHijos.text.toString().toInt()
             var sexo=atvSexo.text.toString()
+            val fotoUri = imageUri?.toString() ?: ""
             //crear objeto de la clase Docente
-            var obj=Docente(cod,nom,pat,mat,sue,hijos,sexo,"")
+            var obj=Docente(cod,nom,pat,mat,sue,hijos,sexo,fotoUri)
             //invocar al método update
             var salida=DocenteController().actualizar(obj)
             //validar salida
@@ -81,6 +94,13 @@ class DatosDocenteActivity : AppCompatActivity() {
                 showAlert("Docente eliminado")
             else
                 showAlert("Error en la eliminación")
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            imageUri = data?.data
+            // Aquí podrías mostrar la imagen seleccionada en un ImageView si lo deseas
         }
     }
     fun showAlert(mensajeAlerta: String) { //mensaje es un parametro: String (tipo de dato)
@@ -110,6 +130,12 @@ class DatosDocenteActivity : AppCompatActivity() {
         txtSueldo.setText(obj.sueldo.toString())
         txtHijos.setText(obj.hijos.toString())
         atvSexo.setText(obj.sexo,false)
+        //agregar imagen en la variable nada mas
+        // si el campo fotoUri tiene un valor válido, convertirlo a Uri y asignarlo
+        // Convertir el valor del campo fotoUri a Uri (si existe)
+        if (!obj.foto.isNullOrEmpty()) {
+            imageUri = Uri.parse(obj.foto)
+        }
     }
 
 
